4c59ef07d31b0eaf30340925194791919959aff3
nocturne <853172766@qq.com>
2020-04-10 12:55:06 +0800

    [bug fix] remove commonControllerAdvice

--- front-server/src/main/java/org/nocturne/vslab/frontserver/advice/ProjectControllerAdvice.java
+++ front-server/src/main/java/org/nocturne/vslab/frontserver/advice/ProjectControllerAdvice.java
@@ -3,22 +3,19 @@ package org.nocturne.vslab.frontserver.advice;
 import org.nocturne.vslab.frontserver.bean.Result;
 import org.nocturne.vslab.frontserver.exceptiion.project.ProjectNotFoundException;
 import org.nocturne.vslab.frontserver.exceptiion.project.UnknownProjectTypeException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ProjectControllerAdvice {
 
     @ExceptionHandler(value = ProjectNotFoundException.class)
-    @ResponseBody
     public Result projectNotFoundHandler(ProjectNotFoundException e) {
         return new Result(ProjectNotFoundException.CODE, "项目不存在", null);
     }
 
 
     @ExceptionHandler(value = UnknownProjectTypeException.class)
-    @ResponseBody
     public Result unknownProjectTypeHandler(UnknownProjectTypeException e) {
         return new Result(UnknownProjectTypeException.CODE, "项目类型不存在", null);
     }
--- front-server/src/main/java/org/nocturne/vslab/frontserver/advice/UserControllerAdvice.java
+++ front-server/src/main/java/org/nocturne/vslab/frontserver/advice/UserControllerAdvice.java
@@ -1,32 +1,27 @@
 package org.nocturne.vslab.frontserver.advice;
 
 import org.nocturne.vslab.frontserver.bean.Result;
-import org.nocturne.vslab.frontserver.exceptiion.project.UnknownProjectTypeException;
 import org.nocturne.vslab.frontserver.exceptiion.user.UserAuthFailException;
 import org.nocturne.vslab.frontserver.exceptiion.user.UserNotFoundException;
 import org.nocturne.vslab.frontserver.exceptiion.user.UsernameAlreadyExist;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class UserControllerAdvice {
 
 
     @ExceptionHandler(value = UserNotFoundException.class)
-    @ResponseBody
     public Result userNotFoundHandler(UserNotFoundException e) {
         return new Result(UserNotFoundException.CODE, "用户不存在", null);
     }
 
     @ExceptionHandler(value = UsernameAlreadyExist.class)
-    @ResponseBody
     public Result usernameAlreadyExistHandler(UsernameAlreadyExist e) {
         return new Result(UsernameAlreadyExist.CODE, "用户名已存在", null);
     }
 
     @ExceptionHandler(value = UserAuthFailException.class)
-    @ResponseBody
     public Result userAuthFailHandler(UserAuthFailException e) {
         return new Result(UserAuthFailException.CODE, "用户未登录", null);
     }
