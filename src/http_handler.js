e4a92977ea8d2b59c706de2c7adb18df5f030b03
李沛熙 <17231162@buaa.edu.cn>
2020-04-12 20:09:45 +0800

    [container server v1.0] finish solve data

--- src/http_handler.js
+++ src/http_handler.js
@@ -3,42 +3,52 @@ const fs = require('fs')
 function solve_data(data) {
     let obj = ''
     let params = data.split('&')
+    for (let i = 0; i < params.length; i = i + 1) {
+        let key_value = params[i].split('=')
+        obj += key_value[0] + ':"' + key_value[1] + '"'
+        if (i != params.length - 1) {
+            obj += ','
+        }
+    }
+    obj = '{' + obj + '}'
+    obj = eval('(' + obj + ')')
+    return obj
 }
 
 function file_file_struct(data) {
-
+    let obj = solve_data(data)
 }
 
 function file_content(data) {
-
+    let obj = solve_data(data)
 }
 
 function file_update(data) {
-
+    let obj = solve_data(data)
 }
 
 function file_new(data) {
-
+    let obj = solve_data(data)
 }
 
 function file_delete(data) {
-
+    let obj = solve_data(data)
 }
 
 function file_move(data) {
-
+    let obj = solve_data(data)
 }
 
 function file_copy(data) {
-
+    let obj = solve_data(data)
 }
 
 function dir_new(data) {
-
+    let obj = solve_data(data)
 }
 
 function dir_delete(data) {
-
+    let obj = solve_data(data)
 }
 
 module.exports = {
