83d3256ad2a44da6e95ce5a8a9d34b804749eff1
李沛熙 <17231162@buaa.edu.cn>
2020-04-16 21:07:54 +0800

    [terminal-server v1.0] initial server

--- src/index.js
+++ src/index.js
@@ -1,104 +1,37 @@
-const http_server = require('http')
-const server = http_server.createServer()
-const { time } = require('./date.js')
-const handler = require('./http_handler.js')
-const port = 3000
+const pty = require('node-pty');
+const os = require('os');
+const WebSocket = require('ws');
+const { time } = require('./date')
+const shell = os.platform() === 'win32' ? 'powershell.exe' : 'bash';
 
-server.listen(port, function () {
-    console.log('[%s]* service init successfully! Listen at %d', time(), port)
+const wss = new WebSocket.Server({port: 4001});
+
+// console.log('begin')
+
+wss.on('listening', function () {
+    console.log('[%s]* terminal service init success!', time())
 })
 
-server.on('request', function (req, res) {
-    // res.setHeader("Access-Control-Allow-Credentials","true")
-    // res.setHeader("Access-Control-Allow-Origin", req.headers.origin)
-    console.log('[%s]* receive request from %s', time(), req.headers.origin)
-    if (req.method == 'GET') {
-        let response = {}
-        console.log('[%s]* request url is %s', time(), req.url)
-        let data = req.url.split('?')[1]
-        let url = req.url.split('?')[0]
-        try {
-            switch (url) {
-                case '/file/struct':
-                    response = handler.file_struct(data)
-                    break;
-                case '/file/content':
-                    response = handler.file_content(data)
-                    break;
-                default:
-                    response = {
-                        code: -100,
-                        message: 'request path not found!',
-                        data : {}
-                    }
-                    break;
-            }
-        } catch (err) {
-            console.log('[%s]* exception happen when handle request { url: "%s", params: "%s" }', time(), req.url, data)
-            response.code = 500
-            response.message = err.message
-            response.data = {}
-        }
-        console.log('[%s]* response message %s', time(), JSON.stringify(response))
-        res.end(JSON.stringify(response))
-    } else {
-        req.on('data', function(data) {
-            console.log('[%s]* receive data from %s', time(), req.headers.origin)
-            console.log('[%s]* request url is %s', time(), req.url)
-            console.log('[%s]* request data is %s', time(), data.toString())
-            let response = {}
-            try {
-                switch (req.url) {
-                    case '/file/update':
-                        response = handler.file_update(data.toString())
-                        break;
-                    case '/file/new':
-                        response = handler.file_new(data.toString())
-                        break;
-                    case '/file/delete':
-                        response = handler.file_delete(data.toString())
-                        break;
-                    case '/file/move':
-                        response = handler.file_move(data.toString())
-                        break;
-                    case '/file/copy':
-                        response = handler.file_copy(data.toString())
-                        break;
-                    case '/file/rename':
-                        response = handler.file_rename(data.toString())
-                        break;
-                    case '/dir/new':
-                        response = handler.dir_new(data.toString())
-                        break;
-                    case '/dir/delete':
-                        response = handler.dir_delete(data.toString())
-                        break;
-                    case '/dir/move':
-                        response = handler.dir_move(data.toString())
-                        break;
-                    case '/dir/copy':
-                        response = handler.dir_copy(data.toString())
-                        break;
-                    case '/dir/rename':
-                        response = handler.dir_rename(data.toString())
-                        break;
-                    default:
-                        response = {
-                            code: -100,
-                            message: 'request path not found!',
-                            data : {}
-                        }
-                        break;
-                }
-            } catch (err) {
-                console.log('[%s]* exception happen when handle request { url: "%s", params: "%s" }', time(), req.url, data)
-                response.code = 500
-                response.message = err.message
-                response.data = {}
-            }
-            console.log('[%s]* response message %s', time(), JSON.stringify(response))
-            res.end(JSON.stringify(response))
-        })
-    }
-    
+wss.on('close', function () {
+    console.log('wtf')
 })
+
+wss.on('connection', (ws) => {
+  console.log('[%s]* socket connection success', time());
+  const ptyProcess = pty.spawn(shell, [], {
+    name: 'xterm-color',
+    cols: 150,
+    rows: 100,
+    cwd: process.env.HOME,
+    env: process.env
+  });
+  
+  ws.on('message', (res) => {
+    ptyProcess.write(res)
+  });
+  
+  ptyProcess.on('data', function (data) {
+    process.stdout.write(data);
+    ws.send(data)
+  });
+});
--- src/setup.sh
+++
@@ -1,6 +0,0 @@
-mkdir /home
-mkdir /home/project
-mkdir /home/project/bin
-mkdir /home/project/user
-mkdir /home/project/src
-echo "# My project" > /home/project/user/README.md
