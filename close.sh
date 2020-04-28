009dd3f9bb366793616704c7d40057463b33fc15
nocturne <853172766@qq.com>
2020-04-28 08:25:36 +0800

    update close.sh

--- close.sh
+++ close.sh
@@ -1,2 +1,3 @@
 container_id=`head -1 /proc/self/cgroup|cut -d/ -f3|cut -c1-12`
-docker --tlsverify --tlscacert=/home/certs/ca.pem --tlscert=/home/certs/cert.pem --tlskey=/home/certs/key.pem -H tcp://120.53.27.31:2376 stop ${container_id}
+docker -H tcp://${HOST_IP}:2376 stop ${container_id}
+
